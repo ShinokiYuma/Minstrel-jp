3483d5098e397117cace3f1a3a12cb501e5a538f
YumaShinoki <push0910sy@gmail.com>
2025-04-23 18:55:40 +0900

    APIキーを環境変数から取得するように変更

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from showcases.generate import generate
 from showcases.test import test
 from models.openai import Generator
 import streamlit as st
+import os
 
 if __name__ == "__main__":
     state = st.session_state
     if "generator" not in state:
         state.generator = Generator(
-            api_key = "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",
+            api_key = os.environ.get("OPENAI_API_KEY"),
             base_url = "https://api.openai.com/v1"
         )
         state.generator.set_model("gpt-4o")
